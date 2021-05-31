f3266fff6eaca8ce29d3920bb9ffbe627638ca70
l1vingthedream <mikebegic@gmail.com>
2021-05-31 19:54:40 -0400

    simple calculator

---
+++ luka/calculator_simple.py
@@ -0,0 +1,23 @@
+print("Welcome to calculator!\nPlease enter the operation [1-4]:")
+
+while True:
+    op = int(input("[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Division\n[5] EXIT\n"))
+    if op == 5:
+        print("exiting...")
+        quit()
+
+    firstNumber = int(input("please enter your first number: "))
+    secondNumber = int(input("Please enter your second number: "))
+    answer = 0
+
+    if op == 1:
+        answer = firstNumber + secondNumber
+    if op == 2:
+        answer = firstNumber - secondNumber
+    if op == 3:
+        answer = firstNumber * secondNumber
+    if op == 4: 
+        answer = firstNumber / secondNumber
+
+
+    print (answer)
