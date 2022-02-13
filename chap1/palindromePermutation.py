deb5b593c71cf92903450cce4c8fee26b73fdb26
l1vingthedream <mikebegic@gmail.com>
2022-02-13 18:50:54 -0500

    check for non-alpha chars

---
+++ chap1/palindromePermutation.py
@@ -0,0 +1,24 @@
+
+def isPalindromePerm(s: str) -> bool:
+    if len(s) == 0:
+        return False
+
+    charCounter: dict[str, int] = {}
+    for c in s:
+        if c.isalpha():
+            continue
+        c = c.lower
+        if c not in charCounter:
+            charCounter[c] = 1
+        else:
+            charCounter[c] += 1
+    
+    oddCharCount: int = 0
+    for el in charCounter:
+        if (charCounter[el] % 2) == 1:
+            oddCharCount+=1
+    return False if oddCharCount > 1 else True
+
+
+s = input("Enter a string for testing palindrome: ")
+print(str(isPalindromePerm(s)))
